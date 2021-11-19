e660793935e6494b83f5719f418735b832b8353d
chayanbansal9195 <chayanbansal1999@gmail.com>
2021-11-19 19:56:24 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
+                <div className="header_search">
+                    <SearchIcon/>
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="header_right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
